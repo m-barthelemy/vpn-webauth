5114755eb6c018488770f4ec8a0279e1cbee599d
Matthieu Barthélemy <bonsouere@gmail.com>
2020-11-12 07:57:57 +0800

    Refactor embedded assets and add header template

--- routes/routes.go
+++ routes/routes.go
@@ -17,6 +17,8 @@ func New(config *models.Config, db *gorm.DB) http.Handler {
 
 	mux := http.NewServeMux()
 
+	mux.Handle("/assets/", http.FileServer(http.Dir("templates/")))
+
 	tplHandler := NewTemplateHandler(config)
 	// Anything not matching a route below will be considered as a HTML template
 	mux.HandleFunc("/", tplHandler.HandleTemplate)
--- routes/template_handler.go
+++ routes/template_handler.go
@@ -25,10 +25,10 @@ func (g *TemplateHandler) HandleTemplate(response http.ResponseWriter, request *
 		fileName = "index.html"
 	}
 	tmplt := template.New(fileName)
-	tmplt, _ = tmplt.ParseFiles("templates/" + fileName)
+	tmplt, _ = tmplt.ParseFiles("templates/header.html", "templates/"+fileName)
 
 	if err := tmplt.Execute(response, g.config); err != nil {
-		log.Println(err.Error())
+		log.Printf("Error serving template %s: %s", fileName, err.Error())
 		http.Error(response, http.StatusText(500), 500)
 	}
 
