3b00b477cb217b6fa5251b7b1d7f56768ca22e2c
Matthieu Barthélemy <bonsouere@gmail.com>
2020-11-21 19:27:13 +0800

    Set mfa flag in cookie

--- controllers/otp/otp_controller.go
+++ controllers/otp/otp_controller.go
@@ -195,5 +195,12 @@ func (u *OTPController) ValidateOTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	log.Printf("OTPController: User %s created VPN session from %s", email, sourceIP)
+
+	if userManager.CreateSession(user.Email, true, w) != nil {
+		log.Printf("WebAuthNController: Error creating user MFA session for %s: %s", user.Email, err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
 	http.Redirect(w, r, "/success", http.StatusTemporaryRedirect)
 }
