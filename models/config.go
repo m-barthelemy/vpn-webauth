1ec2fc09ce95b5324a1eac6be14c3545707d4ad5
Matthieu Barthelemy <matthieu.barthelemy@kaligo.com>
2021-10-19 08:52:38 +0800

    Add ALLOWEDVPNGWIPS

--- models/config.go
+++ models/config.go
@@ -65,9 +65,9 @@ type Config struct {
 
 func (config *Config) New() Config {
 	var defaultConfig = Config{
-		AllowedVPNGwIPs:        []IPDecoder{
+		AllowedVPNGwIPs: []IPDecoder{
 			// 0.0.0.0/0
-			IPDecoder(net.IPNet{IP: net.IPv4(0x00, 0x00, 0x00, 0x00), Mask: net.CIDRMask(0, 32)})
+			IPDecoder(net.IPNet{IP: net.IPv4(0x00, 0x00, 0x00, 0x00), Mask: net.CIDRMask(0, 32)}),
 		},
 		ConnectionsRetention:   90,
 		DbType:                 "sqlite",
