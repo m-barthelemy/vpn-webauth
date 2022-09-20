06677da323336c40be352cfda849e82747500774
Ethan MCGee <ethanmcgee@iagbenefits.com>
2022-09-19 23:35:34 -0400

    remove CPS since jquery / CSP don't play nice with mobile safari

--- build_hashes.sh
+++ build_hashes.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-CSS="$(cat templates/assets/css.css | openssl dgst -sha384 -binary | openssl base64 -A)"
-FONT_AWESOME="$(cat templates/assets/font-awesome.min-4.7.0.css | openssl dgst -sha384 -binary | openssl base64 -A)"
-JQUERY="$(cat templates/assets/jquery-3.5.1.min.js | openssl dgst -sha384 -binary | openssl base64 -A)"
-MATERIAL="$(cat templates/assets/material-icons.css | openssl dgst -sha384 -binary | openssl base64 -A)"
-MATERIALIZE_JS="$(cat templates/assets/materialize.min-0.97.5.js | openssl dgst -sha384 -binary | openssl base64 -A)"
-MATERIALIZE_CSS="$(cat templates/assets/materialize-0.97.5.min.css | openssl dgst -sha384 -binary | openssl base64 -A)"
-SCRIPT="$(cat templates/assets/script.js | openssl dgst -sha384 -binary | openssl base64 -A)"
+CSS="sha384-$(cat templates/assets/css.css | openssl dgst -sha384 -binary | openssl base64 -A)"
+FONT_AWESOME="sha384-$(cat templates/assets/font-awesome.min-4.7.0.css | openssl dgst -sha384 -binary | openssl base64 -A)"
+JQUERY="sha384-$(cat templates/assets/jquery-3.5.1.min.js | openssl dgst -sha384 -binary | openssl base64 -A)"
+MATERIAL="sha384-$(cat templates/assets/material-icons.css | openssl dgst -sha384 -binary | openssl base64 -A)"
+MATERIALIZE_JS="sha384-$(cat templates/assets/materialize.min-0.97.5.js | openssl dgst -sha384 -binary | openssl base64 -A)"
+MATERIALIZE_CSS="sha384-$(cat templates/assets/materialize-0.97.5.min.css | openssl dgst -sha384 -binary | openssl base64 -A)"
+SCRIPT="sha384-$(cat templates/assets/script.js | openssl dgst -sha384 -binary | openssl base64 -A)"
 
-sed -i "s@<link href=\"/assets/material-icons.css\" rel=\"stylesheet\" integrity=\"[^\"]+\">@<link href=\"/assets/material-icons.css\" rel=\"stylesheet\" integrity=\"$MATERIAL\">@g" templates/header.html
-sed -i "s@<link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/materialize-0.97.5.min.css\" integrity=\"[^\"]+\">@<link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/materialize-0.97.5.min.css\" integrity=\"$MATERIALIZE_CSS\">@g" templates/header.html
-sed -i "s@<link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/font-awesome.min-4.7.0.css\" integrity=\"[^\"]+\">@<link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/font-awesome.min-4.7.0.css\" integrity=\"$FONT_AWESOME\">@g" templates/header.html
-sed -i "s@<link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/css.css\" integrity=\"[^\"]+\">@<link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/css.css\" integrity=\"$CSS\">@g" templates/header.html
-sed -i "s@<script type=\"text/javascript\" src=\"/assets/jquery-3.5.1.min.js\" integrity=\"[^\"]+\"></script>@<script type=\"text/javascript\" src=\"/assets/jquery-3.5.1.min.js\" integrity=\"$JQUERY\"></script>@g" templates/header.html
-sed -i "s@<script type=\"text/javascript\" src=\"/assets/materialize.min-0.97.5.js\" integrity=\"[^\"]+\"></script>@<script type=\"text/javascript\" src=\"/assets/materialize.min-0.97.5.js\" integrity=\"$MATERIALIZE_JS\"></script>@g" templates/header.html
-sed -i "s@<script type=\"text/javascript\" src=\"/assets/script.js\" integrity=\"[^\"]+\"></script>@<script type=\"text/javascript\" src=\"/assets/script.js\" integrity=\"$SCRIPT\"></script>@g" templates/header.html
+sed -i '' "s@.*material-icons.css.*@<link href=\"/assets/material-icons.css\" rel=\"stylesheet\" integrity=\"$MATERIAL\">@g" templates/header.html
+sed -i '' "s@.*materialize-0.97.5.min.css.*@<link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/materialize-0.97.5.min.css\" integrity=\"$MATERIALIZE_CSS\">@g" templates/header.html
+sed -i '' "s@.*font-awesome.min-4.7.0.css.*@<link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/font-awesome.min-4.7.0.css\" integrity=\"$FONT_AWESOME\">@g" templates/header.html
+sed -i '' "s@.*css.css.*@<link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/css.css\" integrity=\"$CSS\">@g" templates/header.html
+sed -i '' "s@.*jquery-3.5.1.min.js.*@<script type=\"text/javascript\" src=\"/assets/jquery-3.5.1.min.js\" integrity=\"$JQUERY\"></script>@g" templates/header.html
+sed -i '' "s@.*materialize.min-0.97.5.js.*@<script type=\"text/javascript\" src=\"/assets/materialize.min-0.97.5.js\" integrity=\"$MATERIALIZE_JS\"></script>@g" templates/header.html
+sed -i '' "s@.*script.js.*@<script type=\"text/javascript\" src=\"/assets/script.js\" integrity=\"$SCRIPT\"></script>@g" templates/header.html
 
 ~/go/bin/pkger
